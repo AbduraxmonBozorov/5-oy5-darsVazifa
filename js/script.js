d597b99e6934ab9d14b4f5acf20272a938644a57
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-06-29 09:59:21 +0500

    vazifa

--- js/script.js
+++ js/script.js
@@ -5,17 +5,38 @@ let dark=document.querySelector(".dark");
 let light=document.querySelector(".light");
 let focus=document.querySelector(".focus");
 let isLight=true;
+let darkModeSection=document.querySelector(".darkMode .section");
+let lightModeSection=document.querySelector(".lightMode .section");
+
+let select=document.querySelector("#lang");
+
+
+light.style.opacity="0.5"
 
 theme.addEventListener("click", ()=>{
     if(isLight){
         focus.classList.remove("left");
         focus.classList.add("right")
         isLight=!isLight;
+        darkModeSection.style.display="none"
+        lightModeSection.style.display="block"
+        dark.style.opacity="0.5";
+        light.style.opacity="1"
+
     }else{
         focus.classList.remove("right");
         focus.classList.add("left")
         isLight=!isLight;
+        darkModeSection.style.display="block"
+        lightModeSection.style.display="none"
+        dark.style.opacity="1";
+        light.style.opacity="0.5"
     }
    
     
+})
+
+
+select.addEventListener("change", (event)=>{
+    console.log(event.target.value);
 })
