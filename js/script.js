a66d8b695b60cf7233b66539fadd07cb0dd2683e
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-06-29 10:47:15 +0500

    vazifa to'liq bajarildi

--- js/script.js
+++ js/script.js
@@ -10,6 +10,14 @@ let lightModeSection=document.querySelector(".lightMode .section");
 
 let select=document.querySelector("#lang");
 
+let darkLeft=document.querySelector(".darkLeft");
+let lightRight=document.querySelector(".lightRight");
+let h1=document.querySelectorAll(".h1");
+let p=document.querySelectorAll(".p");
+let button=document.querySelectorAll(".button")
+
+
+
 
 light.style.opacity="0.5"
 
@@ -38,5 +46,32 @@ theme.addEventListener("click", ()=>{
 
 
 select.addEventListener("change", (event)=>{
-    console.log(event.target.value);
+    if(event.target.value=='uzb'){
+        darkLeft.innerHTML="QORONG'U REJIM"
+        lightRight.innerHTML="Yorug' REJIM"
+        h1[0].innerHTML="Figma bilan ijodkorlik kuchini oching!";
+        p[0].innerHTML="Dizayn o'yiningizni oshirishga tayyormisiz?"
+        button[0].innerHTML="Endi Figma-ni sinab ko'ring"
+        h1[1].innerHTML="Figma bilan ijodkorlik kuchini oching!";
+        p[1].innerHTML="Dizayn o'yiningizni oshirishga tayyormisiz?"
+        button[1].innerHTML="Endi Figma-ni sinab ko'ring"
+    } else if(event.target.value=='eng'){
+        darkLeft.innerHTML="DARK MODE"
+        lightRight.innerHTML="LIGHT MODE"
+        h1[0].innerHTML="Unlock the Power of Creativity with Figma!";
+        p[0].innerHTML="Ready to elevate your design game? "
+        button[0].innerHTML="Try Figma now"
+        h1[1].innerHTML="Unlock the Power of Creativity with Figma!";
+        p[1].innerHTML="Ready to elevate your design game? "
+        button[1].innerHTML="Try Figma now"
+    }else if(event.target.value=='rus'){
+        darkLeft.innerHTML="ТЕМНЫЙ РЕЖИМ"
+        lightRight.innerHTML="СВЕТОВОЙ РЕЖИМ"
+        h1[0].innerHTML="Раскройте силу творчества с Figma!";
+        p[0].innerHTML="Готовы улучшить свою дизайнерскую игру?"
+        button[0].innerHTML="Попробуйте Фигму прямо сейчас"
+        h1[1].innerHTML="Раскройте силу творчества с Figma!";
+        p[1].innerHTML="Готовы улучшить свою дизайнерскую игру?"
+        button[1].innerHTML="Попробуйте Фигму прямо сейчас"
+    }
 })
